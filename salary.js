2b7502b5759f1c6b43939d93f9be3db20db2ad49
Christiana Routon <christiana.routon@gmail.com>
2018-02-03 18:08:05 -0600

    added comments

--- salary.js
+++ salary.js
@@ -6,23 +6,8 @@ $(document).ready(function(){
 
 var employeeArray = []
 
-// function checkIds(){
-//   console.log ('in check ids')
-//    if (employeeArray.length != 0){
-//      console.log('in if statement');
-//   // for (var i=0; i<employeeArray.length; i++){
-//   //   if ($('#employeeId').val('') == employeeArray[i].employeeId){
-//   //     alert("Employee must have unique ID");
-//   //     return false;
-//   //   }
-//   // }
-//    }
-//    else{
-//    gatherEmployeeInformation();}
-//    //return true;
-// }
-
 function gatherEmployeeInformation(){
+  //add new employee object based on input
   var newEmployee = {
     firstName: $('#firstName').val(),
     lastName: $('#lastName').val(),
@@ -30,30 +15,30 @@ function gatherEmployeeInformation(){
     jobTitle: $('#jobTitle').val(),
     salary: $('#annualSalary').val()
   }
+  //check to make sure IDs are unique
   for (var i=0; i<employeeArray.length; i++){
     if ($('#employeeId').val() == employeeArray[i].employeeId){
       alert("Employee ID must be unique");
       $('#employeeId').val('');
-      return;
+      return; //stop function if IDs are not unique
     }
   }
+  //add new employee object to employeeArray
   employeeArray.push(newEmployee);
+  //clear input values
   $('#employeeId').val('');
   $('#firstName').val('');
   $('#lastName').val('');
   $('#jobTitle').val('');
   $('#annualSalary').val('');
-  console.log(employeeArray);
-  //call on displayEmployees function
-  //displayEmployees();
-  //calculate total salary
+  //call on calculateCost to calculate total salaries
   calculateCost();
-  }
+} //gatherEmployeeInformation
 
 function displayEmployees(newSalary){
-  console.log('in displayEmployees')
+  //empty table body
   $('#employeeTableBody').empty();
-  var toAppendToTable;
+  var toAppendToTable;  //start of what will be appended to table body--employeesArray as table cells
   for (var i=0; i<employeeArray.length; i++){
       toAppendToTable += '<tr>';
       toAppendToTable += '<td>';
@@ -72,7 +57,7 @@ function displayEmployees(newSalary){
       toAppendToTable += employeeArray[i].salary;
       toAppendToTable += '</td>';
       toAppendToTable += '<td>';
-      toAppendToTable += '<button id="deleteButton" data-salary="' +i+ '">Delete</button>';
+      toAppendToTable += '<button id="deleteButton" data-index="' +i+ '">Delete</button>';
       toAppendToTable += '</td>';
       toAppendToTable += '</tr>';
     }
@@ -90,8 +75,8 @@ function displayEmployees(newSalary){
     toAppendToTable += newSalary;
     toAppendToTable += '</td>';
     toAppendToTable += '</tr>';
-    console.log("toAppendToTable:", toAppendToTable);
     $('#employeeTableBody').append(toAppendToTable);
+    //css for dyanmic table body
     $('td').css("border-style", "solid");
     $('td').css("border-width", "1px");
     $('td').css("border-color", "#0E1828");
@@ -107,23 +92,22 @@ function displayEmployees(newSalary){
 }
 
 function calculateCost(){
-  console.log('in calculateCost',employeeArray);
   var salaryTotal = 0;
   for (var i=0; i<employeeArray.length; i++){
+    //add each employee object's salary to salaryTotal
     salaryTotal+=parseInt(employeeArray[i].salary);
   }
-  //return (salaryTotal);
-  //display new salary on DOM
+  //display employees on DOM
   displayEmployees(salaryTotal);
-}
+}//end calculateCost
 
 function deleteButtonPressed(){
-  console.log( "in deleteButtonPressed", $( this ).data( 'salary' ) );
+  //remove table row
   $(this).parent().parent().remove();
-  //remove employee from employeesArray
-  var index = $( this ).data( 'salary' )
-  //console.log(employeeArray[index]);
+  //index of employee object to be deleted
+  var index = $( this ).data( 'index' );
+  //take employee out of employeeArray
   employeeArray.splice( index , 1 );
   //recalculate salary
   calculateCost();
-}
+}//end deleteButtonPressed
